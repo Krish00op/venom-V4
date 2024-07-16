715eefd230a293a2011c3c8033be3402a93527a5
Krish00op <krishna.bhalotia.2021@gmail.com>
2024-07-16 23:04:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7103285870:AAFMRchdCpDfjfFsjaxBuu-XDiuSCZ5kn7w")
+BOT_TOKEN=("7403832484:AAHaWDk4qsG7i82FtOiQghUtjApF_CeB0Z0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("karanarjun80")
+OWNER_USERNAME=("Krishna Bhalotia")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1609642320")
+ADMIN_IDS=("1444264366")
 
 
 
